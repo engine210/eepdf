2baa71c2fa562c3d3655c4bf8dd9174b016a9ef2
Stevn Wang <engine210@gmail.com>
2021-09-22 00:56:02 +0800

    format

--- src/js/popup.js
+++ src/js/popup.js
@@ -7,14 +7,14 @@ var update = function(val) {
 };
 
 function save_imgs_to_pdf(msg) {
-	urls = msg.urls; // array of images' url
-	filename = msg.filename;
-	console.log('onResponse', urls);
-	console.log('onResponse', filename);
+    urls = msg.urls; // array of images' url
+    filename = msg.filename;
+    console.log('onResponse', urls);
+    console.log('onResponse', filename);
 
-	// urls is the array of images' url
+    // urls is the array of images' url
     var img1 = new Image();
-	// when first image is loaded, get it's height and width
+    // when first image is loaded, get it's height and width
     img1.onload = function() {
         var pdf_height = this.height;
         var pdf_width = this.width;
@@ -22,17 +22,17 @@ function save_imgs_to_pdf(msg) {
         console.log('Image heigth:', pdf_height);
         console.log('Image width:', pdf_height);
 
-		// create a pdf document, set pdf width and height to image size
+        // create a pdf document, set pdf width and height to image size
         var doc = new jsPDF({
             orientation : 'landscape',
             unit : 'px',
             format : [ pdf_width, pdf_height ]
         });
 
-		// add first image to pdf
+        // add first image to pdf
         doc.addImage(this, 'JPEG', 0, 0, pdf_width, pdf_height);
 
-		// add rest of images to pdf
+        // add rest of images to pdf
         for (var i = 1; i < urls.length; i++) {
             console.log('Processing image', i, '/', urls.length);
 
@@ -44,31 +44,26 @@ function save_imgs_to_pdf(msg) {
 
         doc.save(filename + '.pdf');
     };
-	// load first image
+    // load first image
     img1.src = urls[0];
 }
 
 Button.addEventListener('click', function(ce) {
     console.log('clicked');
 
-	document.getElementById("status").textContent = 'Processing...';
+    document.getElementById("status").textContent = 'Processing...';
     chrome.tabs.query({active : true, currentWindow : true}, function(tabs) {
-		var tab = tabs[0];
+        var tab = tabs[0];
         console.log(tab.url, tab.title);
         chrome.tabs.getSelected(null, function(tab) {
-			try{
-				if (typeof variable !== 'undefined') {
-					chrome.tabs.sendMessage(tab.id, {text : 'get_img_urls'}, function(msg) {
-						save_imgs_to_pdf(msg);
-						document.getElementById("status").textContent = 'Done';
-					});
-				} else {
-					document.getElementById("status").textContent = 'Download not availiable';
-				}
-			}catch(err){
+			if (typeof variable !== 'undefined') {
+				chrome.tabs.sendMessage(tab.id, {text : 'get_img_urls'}, function(msg) {
+					save_imgs_to_pdf(msg);
+					document.getElementById("status").textContent = 'Done';
+				});
+			} else {
 				document.getElementById("status").textContent = 'Download not availiable';
 			}
-			
         });
     });
 });
