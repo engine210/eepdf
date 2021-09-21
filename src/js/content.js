ad0d0cf841964abb0f3cafd72724b56a0df66250
Stevn Wang <engine210@gmail.com>
2021-09-22 02:38:04 +0800

    fix error handling bug

--- src/js/content.js
+++ src/js/content.js
@@ -1,5 +1,3 @@
-console.log('i m content.js')
-
 function get_img_urls() {
     var ptt = document.querySelector("#pptContainer").children[0];
     const listArray = Array.from(ptt.children);
@@ -14,13 +12,19 @@ function get_img_urls() {
 }
 
 // Listen for messages
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     if (msg.text === 'get_img_urls') {
         var ret = {};
-        console.log('blablabla');
-        ret['urls'] = get_img_urls();
-        console.log('blablabla', ret['urls']);
-        ret['filename'] = document.title.replace(' | 國立清華大學 eeclass 數位學習平台', '');
-        sendResponse(ret);
+        if (document.querySelector("#pptContainer") == null) {
+            ret['available'] = false;
+            ret['urls'] = null;
+            ret['filename'] = document.title.replace(' | 國立清華大學 eeclass 數位學習平台', '');
+            sendResponse(ret);
+        } else {
+            ret['available'] = true;
+            ret['urls'] = get_img_urls();
+            ret['filename'] = document.title.replace(' | 國立清華大學 eeclass 數位學習平台', '');
+            sendResponse(ret);
+        }
     }
 });
